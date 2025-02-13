dfc9d7fcf4cf655282dfd75f91abf900f89680c9
nyaous555 <>
2025-02-13 11:30:47 +0900

    firestoreモジュールのインポート、初期化

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD2CYwBr_Yr-B7v-9bEanZMBqCGaW6PRac",
+  authDomain: "siid-step10.firebaseapp.com",
+  projectId: "siid-step10",
+  storageBucket: "siid-step10.firebasestorage.app",
+  messagingSenderId: "755688156923",
+  appId: "1:755688156923:web:d4667c36944e71dd240a5e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
